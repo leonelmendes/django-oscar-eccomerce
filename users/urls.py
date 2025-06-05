fba002dec6c3681e1235509705470a02fe5f5233
Leonel Francisco <leonelftacisco@hotmail.com>
2025-06-05 22:03:59 +0100

    Delete users directory

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ClienteRegisterView, FornecedorRegisterView
-
-urlpatterns = [
-    path('register/cliente/', ClienteRegisterView.as_view(), name='register-cliente'),
-    path('register/fornecedor/', FornecedorRegisterView.as_view(), name='register-fornecedor'),
-]
